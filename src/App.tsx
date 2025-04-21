f8738bb12c931644f3c2a9252f26f9bdc90386de
bn-madhu <madhugowda.588@gmail.com>
2025-04-21 12:09:22 +0530

    added minimum body with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ChatBotIcon from './components/ChatBotIcon'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="chatbot-popup">
+          {/* chatBot header */}
+          <div className="chat-header">
+            <div className="header-info">
+              <ChatBotIcon />
+              <h2 className="logo-text">Chat bot</h2>
+            </div>
+            <button className="material-symbols-rounded">keyboard_arrow_up</button>
+          </div>
+          {/* chatbot body */}
+          <div className="chat-body">
+            <div className="message bot-message">
+              <ChatBotIcon />
+              <p className="message-text">
+                Hey there, How can I help you today?
+              </p>
+            </div>
+            <div className="message user-message">
+              <p className="message-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quis numquam fugiat ea laborum ratione maiores omnis architecto delectus, enim, exercitationem eligendi ullam. Quis ipsam aperiam nihil nobis. Eaque, totam?</p>
+            </div>
+          </div>
+          {/* chatBot footer */}
+          <div className="chat-footer">
+            <form action="#" className="chat-form">
+              <input type="text" placeholder='Message.....' className='message-input' required />
+              <button className='material-symbols-rounded'>arrow_upward</button>
+            </form>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
