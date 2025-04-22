29d66be0e0ca4b929257449b08e80388a3533979
bn-madhu <madhugowda.588@gmail.com>
2025-04-22 11:00:24 +0530

    added chat model working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import ChatBotIcon from './components/ChatBotIcon'
+import ChatForm from './components/ChatForm'
+import ChatMessage from './components/ChatMessage';
+import Loader from './components/Loader';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [chatHistory, setChatHistory] = useState<String[]>([]);
+  const chatBotref = useRef<HTMLDivElement>(null);
 
+  type Message = {
+    role: string;
+    text: string;
+  };
+
+  const generateBotresponse = async(history:Message) => {
+    const updateHistory = (text:Message) => {
+      setChatHistory((prev):any => [...prev.filter((msg):any=>msg?.text !== "Thinking..."), {role:"model",text}]);
+    }
+    // console.log(history, chatHistory);
+    history = history.map(({role,text}:Message) => ({role, parts: [{text}]}));
+
+    const requestOptionsPayload = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({contents: history})
+    }
+
+    try {
+      const response = await fetch(import.meta.env.VITE_GOOGLE_API_URL, requestOptionsPayload);
+      const data = await response.json();
+      if(!response.ok) throw new Error(data.error.message || "Something went wrong!!!")
+        // console.log(data);
+
+      // Clean and update chat history with bot's response
+      const apiResponse = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+      updateHistory(apiResponse);
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
+  useEffect(
+    ()=>{
+      chatBotref?.current?.scrollTo({top: chatBotref.current?.scrollHeight, behavior: "smooth"})
+    },[chatHistory]
+  )
   return (
     <>
+    {/* <Loader /> */}
       <div className="container">
         <div className="chatbot-popup">
           {/* chatBot header */}
@@ -15,26 +58,26 @@ function App() {
               <ChatBotIcon />
               <h2 className="logo-text">Chat bot</h2>
             </div>
-            <button className="material-symbols-rounded">keyboard_arrow_up</button>
+            <button className="material-symbols-rounded">keyboard_arrow_down</button>
           </div>
           {/* chatbot body */}
-          <div className="chat-body">
+          <div ref={chatBotref} className="chat-body">
             <div className="message bot-message">
               <ChatBotIcon />
               <p className="message-text">
-                Hey there, How can I help you today?
+                Hey there, <br /> How can I help you today?
               </p>
             </div>
-            <div className="message user-message">
-              <p className="message-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quis numquam fugiat ea laborum ratione maiores omnis architecto delectus, enim, exercitationem eligendi ullam. Quis ipsam aperiam nihil nobis. Eaque, totam?</p>
-            </div>
+            {/* Rebder the chat dynamically */}
+            {
+              chatHistory.map((chat,index) => (
+                <ChatMessage key={index} chat={chat} />
+              ))
+            }
           </div>
           {/* chatBot footer */}
           <div className="chat-footer">
-            <form action="#" className="chat-form">
-              <input type="text" placeholder='Message.....' className='message-input' required />
-              <button className='material-symbols-rounded'>arrow_upward</button>
-            </form>
+            <ChatForm chatHistory={chatHistory} setChatHistory={setChatHistory} generateBotresponse={generateBotresponse} />
           </div>
         </div>
       </div>
